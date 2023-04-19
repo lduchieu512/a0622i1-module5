2e84fc18f4611e708e047b546c1a4182ab3495e0
lduchieu512 <lduchieu512@gmail.com>
2023-04-19 21:07:17 +0700

    tuan2

--- module6/ss3/ss3-module5/src/app/app.module.ts
+++ module6/ss3/ss3-module5/src/app/app.module.ts
@@ -14,6 +14,14 @@ import { Ss5ThBai1Component } from './ss5/ss5-th-bai1/ss5-th-bai1.component';
 import { Ss5ThBai2Component } from './ss5/ss5-th-bai2/ss5-th-bai2.component';
 import { Ss6ThBai1Component } from './ss6/ss6-th-bai1/ss6-th-bai1.component';
 import { Ss3BtBai2Component } from './ss3/ss3-bt-bai2/ss3-bt-bai2.component';
+import { ArticleComponent } from './ss4/ss4-bt-bai1/article/article.component';
+import { FooterComponent } from './ss4/ss4-bt-bai1/footer/footer.component';
+import { HeaderComponent } from './ss4/ss4-bt-bai1/header/header.component';
+import { NavbarComponent } from './ss4/ss4-bt-bai1/navbar/navbar.component';
+import { LikesComponent } from './ss4/ss4-bt-bai1/likes/likes.component';
+import { Ss5BtBai1Component } from './ss5/ss5-bt-bai1/ss5-bt-bai1.component';
+import { CountdownTimerComponent } from './ss5/ss5-bt-bai1/countdown-timer/countdown-timer.component';
+import { RatingBarComponent } from './ss5/ss5-bt-bai1/rating-bar/rating-bar.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +36,15 @@ import { Ss3BtBai2Component } from './ss3/ss3-bt-bai2/ss3-bt-bai2.component';
     Ss5ThBai1Component,
     Ss5ThBai2Component,
     Ss6ThBai1Component,
-    Ss3BtBai2Component
+    Ss3BtBai2Component,
+    ArticleComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    LikesComponent,
+    Ss5BtBai1Component,
+    CountdownTimerComponent,
+    RatingBarComponent
   ],
     imports: [
         BrowserModule,
